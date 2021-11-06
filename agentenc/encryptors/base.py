68274af1da3273f678a35bf8632e17018e502f7c
jm12138 <2286040843@qq.com>
2021-11-07 00:56:06 +0800

    Add interval encrypt

--- agentenc/encryptors/base.py
+++ agentenc/encryptors/base.py
@@ -50,31 +50,36 @@ class BaseEncryptor:
                 warpper(func): encrypt or decrypt func warpper
             '''
             if mode == 'encrypt':
-                def warpper(obj, input: bytes, compress: bool = True, **kwargs) -> bytes:
+                def warpper(obj, input: bytes, compress: bool = False, interval: int = 1, **kwargs) -> bytes:
                     '''
                     encrypt func warpper
 
                     param:
-                        input(bytes): input data
-                        compress(bool: True): if compress data
+                        input(bytes len(input)[1-256^6]): input data
+                        compress(bool: False): if compress data
+                        interval(int: 1 [1-65535]): the interval of input data to encrypt, the recommended interval for text data is 1, means all are encrypted.
                         **kwargs: some other params of encrypt func
 
                     return:
                         output(bytes): output data
                     '''
-                    output = func(obj, input)
+                    assert isinstance(interval, int) and (1 <= interval <= 65535), 'interval should in 1-65535'
+                    assert len(input) <= 256**6, 'input data is much large.'
+                    output = func(obj, input[::interval])
+                    length = len(output).to_bytes(6, 'big')
+                    output = interval.to_bytes(2, 'big') + length + output + b''.join([v.to_bytes(1, 'big') for i, v in enumerate(input) if i % interval!=0])
                     if compress:
                         output = lzma.compress(output)
                     return output
 
             elif mode == 'decrypt':
-                def warpper(input: bytes, compress: bool = True, **kwargs) -> bytes:
+                def warpper(input: bytes, compress: bool = False, **kwargs) -> bytes:
                     '''
                     decrypt func warpper
 
                     param:
                         input(bytes): input data
-                        compress(bool: True): if compress data
+                        compress(bool: False): if compress data
                         head_len(int: 8): len of the head
                         **kwargs: some other params of encrypt func
 
@@ -83,7 +88,14 @@ class BaseEncryptor:
                     '''
                     if compress:
                         input = lzma.decompress(input)
-                    output = func(input, **kwargs)
+                    interval = int.from_bytes(input[:2], 'big')
+                    length = int.from_bytes(input[2:8], 'big')
+                    _output = func(input[8: length+8], **kwargs)
+                    _data = input[length+8:]
+                    output = b''
+                    for i, v in enumerate(_output):
+                        output += v.to_bytes(1, 'big') + _data[i*(interval-1):(i+1)*(interval-1)]
+
                     return output
             return warpper
         return warpper
